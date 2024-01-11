495a4483dabef2c08ad7e1b6081b1d0c4b02b6c8
Annsrutto <annsrutto567@gmail.com>
2024-01-12 02:00:31 +0300

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-""" this module contains class BaseModel that defines all common attributes/methods for other classes"""
+""" this module contains class BaseModel
+that defines all common attributes/methods for other classes"""
 
 from datetime import datetime
 import uuid
 
 
 class BaseModel:
-	""" class that defines all common attributes/methods for other classes"""
-	def __init__(self, id, created_at, updated_at):
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = created_at
-	
-	def __str__(self):
-		""" Returns a string represantation of BaseModel"""
-		return f"[{self.class name}] ({self.id}) {self.__dict__}"
-	
-	def save(self):
-		self.updated_at = datetime.now
-	
-	def to_dict(self):
-		mydict_ = self.__dict__.copy()
-		mydict_("__class__") = self.__class__.name
-		mydict_("created_at") = mydict_("created_at").isoformat()
-		mydict_("updated_at") = mydict_("updated_at").isoformat()
-	    return mydict_
+    """ class that defines all common attributes/methods for other classes"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-	
+    def __str__(self):
+        """ Returns a string represantation of BaseModel"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
