045c7e9f09ec0cc2ccd3ffbc60458f53b871810b
Annsrutto <annsrutto567@gmail.com>
2024-01-12 03:09:13 +0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,18 @@ import uuid
 
 class BaseModel:
     """ class that defines all common attributes/methods for other classes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ('created_at, updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """ Returns a string represantation of BaseModel"""
