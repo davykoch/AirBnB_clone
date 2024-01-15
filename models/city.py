09d71ac793eca643455a1152166946a7bcb2e8c5
Annsrutto <annsrutto567@gmail.com>
2024-01-16 01:25:35 +0300

    More classes!

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class City that inherit from BaseModel"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """class City that inherit from BaseModel"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" class State that inherit from BaseModel"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """class State that inherit from BaseModel"""
+    name = ""
